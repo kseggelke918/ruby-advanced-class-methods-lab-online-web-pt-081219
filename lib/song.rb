38bb5059a099bb72580136007fc37657b314e1f7
Katie Seggelke <katie.seggelke@gmail.com>
2019-09-02 21:58:23 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -35,7 +35,7 @@ class Song
   
   def self.find_or_create_by_name(song_name)
     if @@all.include?(song_name)
-       
+      self.class.find_by_name 
     else 
       self.class.create_by_name
   end 
